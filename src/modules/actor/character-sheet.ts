df6403a5079e46feff1c3ce27c7555ac48f570b6
HeikaHaku <admin@omnious.org>
2020-10-30 14:07:08 -0500

    .

--- src/modules/actor/character-sheet.ts
+++ src/modules/actor/character-sheet.ts
@@ -6,7 +6,7 @@ export class CharacterSheet extends ActorSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ['carrot-royale', 'character-sheet'],
-      template: 'systems/fvtt-carrot-royal/templates/character-sheet.html',
+      template: 'systems/fvtt-carrot-royal/templates/actor/character-sheet.html',
       width: 600,
       height: 400,
       tabs: [
