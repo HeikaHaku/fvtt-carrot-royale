e4e0f6b7b148bd4ad026a983c64e7a90c607cfc4
HeikaHaku <admin@omnious.org>
2020-10-30 19:37:49 -0500

    style: Renamed Character to Hero.

--- src/modules/actor/hero-sheet.ts
+++ src/modules/actor/hero-sheet.ts
@@ -2,11 +2,11 @@
  * @extends {ActorSheet}
  */
 
-export class CharacterSheet extends ActorSheet {
+export class HeroSheet extends ActorSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ['carrot-royale', 'character-sheet'],
-      template: 'systems/fvtt-carrot-royal/templates/actor/character-sheet.html',
+      classes: ['carrot-royale', 'hero-sheet'],
+      template: 'systems/fvtt-carrot-royal/templates/actor/hero-sheet.html',
       width: 600,
       height: 400,
       tabs: [
