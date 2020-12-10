6b7ea3b7b2353756ccd47cfd59f00e8b87b8bd76
HeikaHaku <admin@omnious.org>
2020-12-09 23:08:00 -0600

    feat: Added Mod Calc Handlebar Helper

--- src/modules/registerHandlebarHelpers.ts
+++ src/modules/registerHandlebarHelpers.ts
@@ -6,4 +6,6 @@ Hooks.once('ready', async () => {
   Handlebars.registerHelper('concat', (arg1: string, arg2: string) => ''.concat(arg1, arg2));
 
   Handlebars.registerHelper('firstUpper', (arg: string) => arg.charAt(0).toUpperCase() + arg.slice(1));
+
+  Handlebars.registerHelper('modCalc', (arg1: number) => (arg1 < 10 ? '-' : '+') + Math.floor((arg1 - 10) / 2));
 });
