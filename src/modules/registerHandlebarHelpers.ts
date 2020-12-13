a6ba9459beb7893926823bb2c62557c70fdc6b25
HeikaHaku <admin@omnious.org>
2020-12-13 03:01:15 -0600

    fix: Handlebars fix.

--- src/modules/registerHandlebarHelpers.ts
+++ src/modules/registerHandlebarHelpers.ts
@@ -7,7 +7,7 @@ Hooks.once('ready', async () => {
 
   Handlebars.registerHelper('firstUpper', (arg: string) => arg.titleCase());
 
-  Handlebars.registerHelper('modCalc', (arg: number) => (arg < 10 ? '-' : '+') + Math.floor((arg - 10) / 2));
+  Handlebars.registerHelper('modCalc', (arg: number) => (arg >= 10 ? '+' : '') + Math.floor((arg - 10) / 2));
 
   Handlebars.registerHelper('equipped', (arg: boolean) => (arg ? 'Equipped' : 'Not Equipped'));
 
