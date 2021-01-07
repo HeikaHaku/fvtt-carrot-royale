844b6dc8c147d7cc1e82c9db37938888d9d7ed00
HeikaHaku <admin@omnious.org>
2021-01-07 11:43:23 -0600

    feat: Race Features Config.

---
+++ src/modules/raceFeatures.ts
@@ -0,0 +1,55 @@
+export const RaceFeatures = {
+  orc: {
+    bonus: {
+      stats: {
+        movement: 1,
+        ac: -2,
+        str: 10,
+      },
+    },
+  },
+  human: {},
+  dwarf: {
+    bonus: {
+      stats: {
+        movement: -1,
+        ac: 2,
+      },
+    },
+  },
+  elf: {
+    bonus: {
+      stats: {
+        init: 4,
+        hp: -10,
+      },
+    },
+  },
+  gnome: {
+    bonus: {
+      stats: {
+        movement: -1,
+        ac: -2,
+        saves: 2,
+      },
+    },
+  },
+
+  troll: {
+    bonus: {
+      stats: {
+        saves: -5,
+        regen: 2,
+      },
+    },
+  },
+  giant: {
+    bonus: {
+      stats: {
+        damage: 3,
+        ac: -2,
+        init: -12,
+      },
+    },
+  },
+};
